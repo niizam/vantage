73cda9c1a155563db0e518c2ed5dfc7a29bc3c7a
Nizam <45286708+niizam@users.noreply.github.com>
2022-01-14 17:47:13 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+cp ./icon.png /usr/share/icons/hicolor/scalable/apps/vantage.png
+cp ./vantage.desktop /usr/share/applications/vantage.desktop
+cp ./vantage.sh /usr/bin/vantage
+chmod a+rx /usr/bin/vantage
