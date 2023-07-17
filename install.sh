193639573d95bbb2e0d18fb5ddb0c6ff6a321ae9
niizam <45286708+niizam@users.noreply.github.com>
2023-07-17 20:56:39 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,31 @@
-cp ./icon.png /usr/share/icons/hicolor/scalable/apps/vantage.png
-cp ./vantage.desktop /usr/share/applications/vantage.desktop
-cp ./vantage.sh /usr/bin/vantage
-chmod a+rx /usr/bin/vantage
+#!/bin/bash
+
+# check for the distro
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    distro=$ID
+fi
+
+case $distro in
+  "arch" | "manjaro")
+    echo "Installing on Arch Linux or derivative"
+    pacman -Qi zenity xorg-xinput networkmanager &> /dev/null || sudo pacman -S zenity xorg-xinput networkmanager
+    ;;
+
+  "ubuntu" | "debian" | "linuxmint" | "pop" | "elementary")
+    echo "Installing on Debian or derivative"
+    dpkg -s zenity xinput &> /dev/null || sudo apt install zenity xinput
+    ;;
+
+  "fedora")
+    echo "Installing on Fedora"
+    rpm -q zenity xinput NetworkManager pipewire-pulseaudio &> /dev/null || sudo dnf install zenity xinput NetworkManager pipewire-pulseaudio
+    ;;
+
+  *)
+    echo "Unknown Distro, exiting."
+    exit 1
+    ;;
+esac
+
+echo "Installation successful."
