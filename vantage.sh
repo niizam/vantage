b840170b99070172657aa71fa3e516fbc2b07154
Lanchon <lanchon@gmail.com>
2024-01-01 18:15:28 -0300

    Encapsulate main code in a function

--- vantage.sh
+++ vantage.sh
@@ -58,81 +58,91 @@ show_submenu_on_off() {
     show_submenu "$@" "$SUBMENU_ON" "$SUBMENU_OFF"
 }
 
-while :; do
+main() {
+    while :; do
+        local menu="$(zenity --height 350 --width 350 --list --title "Lenovo Vantage" --text "Select function:" --column "Function" --column "Status" \
+            "Conservation Mode" "$(get_conserv_mode_status)" \
+            "Always-On USB" "$(get_usb_charging_status)" \
+            "Fan Mode" "$(get_fan_mode_status)" \
+            "FN Lock" "$(get_fn_lock_status)" \
+            "Camera" "$(get_camera_status)" \
+            "Microphone" "$(get_microphone_status)" \
+            "Touchpad" "$(get_touchpad_status)" \
+            "WiFi" "$(get_wifi_status)" \
+        )"
+        case "$menu" in
+            "Conservation Mode")
+                local submenu="$(show_submenu_on_off "Conservation Mode" "$(get_conserv_mode_status)")"
+                case "$submenu" in
+                    "$SUBMENU_ON") echo "1" | pkexec tee $vpc/conservation_mode ;;
+                    "$SUBMENU_OFF") echo "0" | pkexec tee $vpc/conservation_mode ;;
+                esac
+                ;;
+            "Always-On USB")
+                local submenu="$(show_submenu_on_off "Always-On USB" "$(get_usb_charging_status)")"
+                case "$submenu" in
+                    "$SUBMENU_ON") echo "1" | pkexec tee $vpc/usb_charging ;;
+                    "$SUBMENU_OFF") echo "0" | pkexec tee $vpc/usb_charging ;;
+                esac
+                ;;
+            "Fan Mode")
+                local submenu="$(show_submenu "Fan Mode" "$(get_fan_mode_status)" \
+                    "Super Silent" \
+                    "Standard" \
+                    "Dust Cleaning" \
+                    "Efficient Thermal Dissipation" \
+                )"
+                case "$submenu" in
+                    "Super Silent") echo "0" | pkexec tee $vpc/fan_mode ;;
+                    "Standard") echo "1" | pkexec tee $vpc/fan_mode ;;
+                    "Dust Cleaning") echo "2" | pkexec tee $vpc/fan_mode ;;
+                    "Efficient Thermal Dissipation") echo "4" | pkexec tee $vpc/fan_mode ;;
+                esac
+                ;;
+            "FN Lock")
+                local submenu="$(show_submenu_on_off "FN Lock" "$(get_fn_lock_status)")"
+                case "$submenu" in
+                    "$SUBMENU_ON") echo "0" | pkexec tee $vpc/fn_lock ;;
+                    "$SUBMENU_OFF") echo "1" | pkexec tee $vpc/fn_lock ;;
+                esac
+                ;;
+            "Camera")
+                local submenu="$(show_submenu_on_off "Camera" "$(get_camera_status)")"
+                case "$submenu" in
+                    "$SUBMENU_ON") pkexec modprobe uvcvideo ;;
+                    "$SUBMENU_OFF") pkexec modprobe -r uvcvideo ;;
+                esac
+                ;;
+            "Microphone")
+                local submenu="$(show_submenu "Microphone" "$(get_microphone_status)" \
+                    "Mute" \
+                    "Unmute" \
+                )"
+                case "$submenu" in
+                    "Mute") pactl set-source-mute @DEFAULT_SOURCE@ 1 ;;
+                    "Unmute") pactl set-source-mute @DEFAULT_SOURCE@ 0 ;;
+                esac
+                ;;
+            "Touchpad")
+                local submenu="$(show_submenu_on_off "Touchpad" "$(get_touchpad_status)")"
+                case "$submenu" in
+                    "$SUBMENU_ON") xinput enable "$touchpad_id" ;;
+                    "$SUBMENU_OFF") xinput disable "$touchpad_id" ;;
+                esac
+                ;;
+            "WiFi")
+                local submenu="$(show_submenu_on_off "WiFi" "$(get_wifi_status)")"
+                case "$submenu" in
+                    "$SUBMENU_ON") nmcli radio wifi on ;;
+                    "$SUBMENU_OFF") nmcli radio wifi off ;;
+                esac
+                ;;
+            *)
+                break
+                ;;
+        esac
+    done
+}
 
-file=$(zenity --height 350 --width 350 --list --title "Lenovo Vantage" --text "Select function:" --column "Function" --column "Status" \
-    "Conservation Mode" "$(get_conserv_mode_status)" \
-    "Always-On USB" "$(get_usb_charging_status)" \
-    "Fan Mode" "$(get_fan_mode_status)" \
-    "FN Lock" "$(get_fn_lock_status)" \
-    "Camera" "$(get_camera_status)" \
-    "Microphone" "$(get_microphone_status)" \
-    "Touchpad" "$(get_touchpad_status)" \
-    "WiFi" "$(get_wifi_status)" \
-)
+main "$@"
 
-case "$file" in
-    "Conservation Mode")
-        choice=$(show_submenu_on_off "Conservation Mode" "$(get_conserv_mode_status)")
-        case "$choice" in
-            "$SUBMENU_ON") echo "1" | pkexec tee $vpc/conservation_mode ;;
-            "$SUBMENU_OFF") echo "0" | pkexec tee $vpc/conservation_mode ;;
-        esac
-        ;;
-    "Always-On USB")
-        choice=$(show_submenu_on_off "Always-On USB" "$(get_usb_charging_status)")
-        case "$choice" in
-            "$SUBMENU_ON") echo "1" | pkexec tee $vpc/usb_charging ;;
-            "$SUBMENU_OFF") echo "0" | pkexec tee $vpc/usb_charging ;;
-        esac
-        ;;
-    "Fan Mode")
-        choice=$(show_submenu "Fan Mode" "$(get_fan_mode_status)" "Super Silent" "Standard" "Dust Cleaning" "Efficient Thermal Dissipation")
-        case "$choice" in
-            "Super Silent") echo "0" | pkexec tee $vpc/fan_mode ;;
-            "Standard") echo "1" | pkexec tee $vpc/fan_mode ;;
-            "Dust Cleaning") echo "2" | pkexec tee $vpc/fan_mode ;;
-            "Efficient Thermal Dissipation") echo "4" | pkexec tee $vpc/fan_mode ;;
-        esac
-        ;;
-    "FN Lock")
-        choice=$(show_submenu_on_off "FN Lock" "$(get_fn_lock_status)")
-        case "$choice" in
-            "$SUBMENU_ON") echo "0" | pkexec tee $vpc/fn_lock ;;
-            "$SUBMENU_OFF") echo "1" | pkexec tee $vpc/fn_lock ;;
-        esac
-        ;;
-    "Camera")
-        choice=$(show_submenu_on_off "Camera" "$(get_camera_status)")
-        case "$choice" in
-            "$SUBMENU_ON") pkexec modprobe uvcvideo ;;
-            "$SUBMENU_OFF") pkexec modprobe -r uvcvideo ;;
-        esac
-        ;;
-    "Microphone")
-        choice=$(show_submenu "Microphone" "$(get_microphone_status)" "Mute" "Unmute")
-        case "$choice" in
-            "Mute") pactl set-source-mute @DEFAULT_SOURCE@ 1 ;;
-            "Unmute") pactl set-source-mute @DEFAULT_SOURCE@ 0 ;;
-        esac
-        ;;
-    "Touchpad")
-        choice=$(show_submenu_on_off "Touchpad" "$(get_touchpad_status)")
-        case "$choice" in
-            "$SUBMENU_ON") xinput enable "$touchpad_id" ;;
-            "$SUBMENU_OFF") xinput disable "$touchpad_id" ;;
-        esac
-        ;;
-    "WiFi")
-        choice=$(show_submenu_on_off "WiFi" "$(get_wifi_status)")
-        case "$choice" in
-            "$SUBMENU_ON") nmcli radio wifi on ;;
-            "$SUBMENU_OFF") nmcli radio wifi off ;;
-        esac
-        ;;
-    *)
-        exit
-        ;;
-esac
-
-done
