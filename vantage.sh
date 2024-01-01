3612688716b66264ca81bd679b55b807f526c244
Lanchon <lanchon@gmail.com>
2024-01-01 17:49:24 -0300

    Clean up status handling

--- vantage.sh
+++ vantage.sh
@@ -6,45 +6,41 @@
 vpc="/sys/bus/platform/devices/VPC2004\:*"
 
 get_conserv_mode_status() {
-    cat $vpc/conservation_mode | awk '{print ($1 == "1") ? "Status: On" : "Status: Off"}'
+    cat $vpc/conservation_mode | awk '{print ($1 == "1") ? "On" : "Off"}'
 }
 
 get_usb_charging_status() {
-    cat $vpc/usb_charging | awk '{print ($1 == "1") ? "Status: On" : "Status: Off"}'
+    cat $vpc/usb_charging | awk '{print ($1 == "1") ? "On" : "Off"}'
 }
 
 get_fan_mode_status() {
     cat $vpc/fan_mode | awk '{
-        if ($1 == "133" || $1 == "0") print "Status: Super Silent";
-        else if ($1 == "1") print "Status: Standard";
-        else if ($1 == "2") print "Status: Dust Cleaning";
-        else if ($1 == "4") print "Status: Efficient Thermal Dissipation";
+        if ($1 == "133" || $1 == "0") print "Super Silent";
+        else if ($1 == "1") print "Standard";
+        else if ($1 == "2") print "Dust Cleaning";
+        else if ($1 == "4") print "Efficient Thermal Dissipation";
     }'
 }
 
 get_fn_lock_status() {
-    cat $vpc/fn_lock | awk '{print ($1 == "1") ? "Status: Off" : "Status: On"}'
+    cat $vpc/fn_lock | awk '{print ($1 == "1") ? "Off" : "On"}'
 }
 
 get_camera_status() {
-    lsmod | grep -q 'uvcvideo' && echo "Status: On" || echo "Status: Off"
+    lsmod | grep -q 'uvcvideo' && echo "On" || echo "Off"
 }
 
 get_microphone_status() {
-    pactl get-source-mute @DEFAULT_SOURCE@ | awk '{print ($2 == "yes") ? "Status: Muted" : "Status: Active"}'
+    pactl get-source-mute @DEFAULT_SOURCE@ | awk '{print ($2 == "yes") ? "Muted" : "Active"}'
 }
 
 get_touchpad_status() {
     string="$(xinput list | grep Touchpad | cut -d '=' -f2 | awk '{print $1}')"
-    xinput --list-props "$string" | grep "Device Enabled" | cut -d ':' -f2 | awk '{print ($1 == "1") ? "Status: On" : "Status: Off"}'
+    xinput --list-props "$string" | grep "Device Enabled" | cut -d ':' -f2 | awk '{print ($1 == "1") ? "On" : "Off"}'
 }
 
 get_wifi_status() {
-    nmcli radio wifi | awk '{print ($1 == "enabled") ? "Status: On" : "Status: Off"}'
-}
-
-filter_status() {
-    cut -d ' ' -f1 --complement
+    nmcli radio wifi | awk '{print ($1 == "enabled") ? "On" : "Off"}'
 }
 
 SUBMENU_ON="Activate"
@@ -54,7 +50,7 @@ show_submenu() {
     local title="$1"
     local status="$2"
     shift 2
-    zenity --list --title "$title" --text "$status" --column "Menu" "$@"
+    zenity --list --title "$title" --text "Status: $status" --column "Menu" "$@"
 }
 
 show_submenu_on_off() {
@@ -64,14 +60,14 @@ show_submenu_on_off() {
 while :; do
 
 file=$(zenity --height 350 --width 350 --list --title "Lenovo Vantage" --text "Select function:" --column "Function" --column "Status" \
-    "Conservation Mode" "$(get_conserv_mode_status | filter_status)" \
-    "Always-On USB" "$(get_usb_charging_status | filter_status)" \
-    "Fan Mode" "$(get_fan_mode_status | filter_status)" \
-    "FN Lock" "$(get_fn_lock_status | filter_status)" \
-    "Camera" "$(get_camera_status | filter_status)" \
-    "Microphone" "$(get_microphone_status | filter_status)" \
-    "Touchpad" "$(get_touchpad_status | filter_status)" \
-    "WiFi" "$(get_wifi_status | filter_status)" \
+    "Conservation Mode" "$(get_conserv_mode_status)" \
+    "Always-On USB" "$(get_usb_charging_status)" \
+    "Fan Mode" "$(get_fan_mode_status)" \
+    "FN Lock" "$(get_fn_lock_status)" \
+    "Camera" "$(get_camera_status)" \
+    "Microphone" "$(get_microphone_status)" \
+    "Touchpad" "$(get_touchpad_status)" \
+    "WiFi" "$(get_wifi_status)" \
 )
 
 case "$file" in
